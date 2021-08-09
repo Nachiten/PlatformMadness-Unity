5ed558e98e2b212f87b05669f1d8e04994030e93
Ignacio Baptista [PC] <ibapista@frba.utn.edu.ar>
2021-08-08 21:15:05 -0300

    Agregado raycast de personaje (no funciona)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speedX = 6.0f;
     public float speedY = 350.0f;
-    public float gravity = 4;
+    public float gravity = 4f;
 
     bool tocandoPiso = false;
 
@@ -23,28 +23,41 @@ public class PlayerMovement : MonoBehaviour
 
         rigidBody.velocity = new Vector2(Input.GetAxis("Horizontal") * speedX, rigidBody.velocity.y);
 
-        if (tocandoPiso && Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, speedY);
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
-    { 
-        Vector3 normal = collision.GetContact(0).normal;
-        bool tocaElPisoDeArriba = normal == Vector3.up;
+    {
+        Vector3 origenRayoIzquierda = new Vector3(transform.position.x - transform.localScale.x / 2, transform.position.y, transform.position.z);
+        Vector3 direccionRayo = transform.TransformDirection(Vector3.down);
+
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(origenRayoIzquierda, direccionRayo, out hitInfo, Mathf.Infinity))
+        {
+            Debug.DrawRay(origenRayoIzquierda, direccionRayo * hitInfo.distance, Color.green, 3);
+            Debug.Log("Raycast DID hit");
+        }
+        else
+        {
+            Debug.DrawRay(origenRayoIzquierda, direccionRayo * 30, Color.red, 3);
+            Debug.Log("Raycast DID NOT hit");
+        }
 
         // Solo estoy tocando el piso si el objeto es mapa, y si es desde arriba
-        if (tocaElPisoDeArriba && collision.gameObject.CompareTag("Mapa"))
-        {
-            tocandoPiso = true;
-        }
+        //if (collision.gameObject.CompareTag("Mapa"))
+        //{
+        //    tocandoPiso = true;
+        //}
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Mapa"))
-            tocandoPiso = false;
-    }
+    //private void OnCollisionExit2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Mapa"))
+    //        tocandoPiso = false;
+    //}
 }
 
