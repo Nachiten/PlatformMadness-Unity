efd1d887d10eebbfafcd47a11487b23c4e3ec481
Ignacio Baptista [PC] <ibapista@frba.utn.edu.ar>
2021-08-11 02:56:39 -0300

    Eliminados archivos no usados, fixeado colisiones de jugador

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,10 @@ public class PlayerMovement : MonoBehaviour
     public float speedX = 6.0f;
     public float speedY = 350.0f;
     public float gravity = 4f;
+    public LayerMask layer;
+
+    float offsetX = 0.55f;
+    float offsetY = 0.09f;
 
     private Rigidbody2D rigidBody;
 
@@ -25,42 +29,23 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    
+
     private bool colisionaConPiso() {
+        Vector3 pointA = new Vector3(transform.position.x + offsetX, transform.position.y - transform.localScale.y / 2);
+        Vector3 pointB = new Vector3(transform.position.x - offsetX, transform.position.y - transform.localScale.y / 2 - offsetY);
 
-        float offsetX = 0.26f;
-        float offsetY = 0.25f;
+        Collider2D checkeoDePiso = Physics2D.OverlapArea(pointA, pointB, layer);
 
-        Vector2 origenRayoIzquierda = new Vector3(transform.position.x - transform.localScale.x / 2 + offsetX, transform.position.y);
-        Vector2 origenRayoDerecha = new Vector3(transform.position.x + transform.localScale.x / 2 - offsetX, transform.position.y);
-        Vector2 direccionRayo = transform.TransformDirection(Vector2.down);
-        float rayDistance = transform.localScale.y / 2 + offsetY;
-
-        // Create bitmask
-        int layer = 1 << 6;
-
-        // This would cast rays only against colliders in layer 6.
-        // But instead I want collide against everything except layer 6. The ~ operator does this, it inverts a bitmask.
-        // (layer 6 is the player)
-        layer = ~layer;
-
-        RaycastHit2D hitIzquierda = Physics2D.Raycast(origenRayoIzquierda, direccionRayo, rayDistance, layer);
-        RaycastHit2D hitDerecha = Physics2D.Raycast(origenRayoDerecha, direccionRayo, rayDistance, layer);
-
-        bool tocoRayoIzquierda = hitIzquierda.collider != null && hitIzquierda.collider.gameObject.CompareTag("Mapa");
-        bool tocoRayoDerecha = hitDerecha.collider != null && hitDerecha.collider.gameObject.CompareTag("Mapa");
-
-        if (tocoRayoIzquierda)
-            Debug.DrawRay(origenRayoIzquierda, direccionRayo * rayDistance, Color.green, 3);
-        else
-            Debug.DrawRay(origenRayoIzquierda, direccionRayo * rayDistance, Color.red, 3);
-
-        if (tocoRayoDerecha)
-            Debug.DrawRay(origenRayoDerecha, direccionRayo * rayDistance, Color.green, 3);
-        else
-            Debug.DrawRay(origenRayoDerecha, direccionRayo * rayDistance, Color.red, 3);
+        if (checkeoDePiso != null) {
+            Debug.DrawLine(pointA, new Vector2(pointB.x, pointA.y), Color.green, 5);
+            Debug.DrawLine(pointA, new Vector2(pointA.x, pointB.y), Color.green, 5);
+            Debug.DrawLine(new Vector2(pointB.x, pointA.y), pointB, Color.green, 5);
+            Debug.DrawLine(new Vector2(pointA.x, pointB.y), pointB, Color.green, 5);
+            return true;
+        }
         
-
-        return tocoRayoIzquierda || tocoRayoDerecha;
+        return false;
     }
 }
 
